db836cb8a0ee25e5ca786a8332ee822939e9677e
mgnamdan <danny.ai.hub@gmail.com>
2026-06-09 09:55:20 -0400

    skeleton defined

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# IMPORTS AND HELPER FUNCTIONS
+# FILE FUNCTIONS
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def loadList():
+    pass
+
+
+def saveList():
+    pass
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# LIST FUNCTIONS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def addItems():
+    pass
+
+
+def removeItems():
+    pass
+
+
+def editItems():
+    pass
+
+
+def moveItems():
+    pass
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# PRINT FUNCTIONS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def printOptions():
+    print("")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("")
+    print("                MENU")
+    print("")
+    print("         1. View List")
+    print("         2. Add Item(s)")
+    print("         3. Remove Item(s)")
+    print("         4. Edit Item(s)")
+    print("         5. Move Item(s)")
+    print("         6. Exit")
+    print("")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("")
+
+
+def printList():
+    pass
 
 
 
@@ -9,7 +59,25 @@
 # MAIN FUNCTION DEFINITION
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def main():
-    pass
+    appOn = True
+
+    while appOn:
+        # PUT INTO VALIDATION LOOP
+        printOptions()
+        userChoice = input(" --> ")
+        # ACCOUNT FOR STRINGS AND INTS?
+        if userChoice == "1":
+            printList()
+        elif userChoice == "2":
+            addItems()
+        elif userChoice == "3":
+            removeItems()
+        elif userChoice == "4":
+            editItems()
+        elif userChoice == "5":
+            moveItems()
+        else:
+            appOn = False
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
